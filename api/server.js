f7f997b6b2a132df1da9435f82351aeae80c5609
Pierluigi Caruso <pierluigic97@gmail.com>
2020-02-27 20:03:11 +0100

    Implemented passwor, TODO hashing

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require("express-session");
 const socketIo = require("socket.io");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -19,10 +20,26 @@ const server=app.listen(port, () => {
   console.log("Listening to port " + port);
 });
 
+//socket.io set up
 const io = socketIo(server);
 io.set('origins', '*:*');
+
+//session set up
+const secret = process.env.SECRET || "vC7*EqraC";
+const sessionMiddlewere=session({
+  secret:secret,
+  resave:false,
+  saveUninitialized:false,
+  cookie:{expires:false}
+})
+
+app.use(sessionMiddlewere)
+io.use((socket,next)=>{
+  sessionMiddlewere(socket.request, socket.request.res, next)
+})
+
 let tdb={                               //todo database
-  "Test1":{chat:["old1","old2"],password:""},
+  "Test1":{chat:["old1","old2"],password:"a"},
   "Test2":{chat:["old1","old2"],password:""},
   "room420":{chat:["lol","much funny"],password:""}
 }
@@ -30,30 +47,28 @@ let tdb={                               //todo database
 io.on("connection", socket => {
   console.log("New client connected");
 
-  socket.on('listRooms',()=>{
-    socket.emit('listRooms',{rooms:Object.keys(tdb)})
-  })
 
   socket.on('startRoom',(data)=>{
     const room=data.room;
 
+    if(socket.request.session.authorizedRooms && socket.request.session.authorizedRooms.includes(room)){
+      socket.join(room,()=>{
 
-    socket.join(room,()=>{
+        socket.emit('history',{
+          history:tdb[room].chat
+        })
+    
+        socket.on("chat",(data)=>{
+          tdb[room].chat.push(data.message);
+          io.to(room).emit('chat',data);
+        })
 
-      socket.emit('history',{
-        history:tdb[room].chat
       })
-  
-      socket.on("chat",(data)=>{
-        tdb[room].chat.push(data.message);
-        io.to(room).emit('chat',data);
+
+      socket.on('leaveRoom',()=>{
+        socket.leave(room)
       })
-
-    })
-
-    socket.on('leaveRoom',()=>{
-      socket.leave(room)
-    })
+    }
 
   })
 
@@ -65,6 +80,14 @@ io.on("connection", socket => {
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+app.get("/RoomList", (req, res) => {
+  let data={
+    rooms:Object.keys(tdb)
+  }
+  res.json(data)
+});
 
 app.post("/CreateRoom", (req, res) => {
   let data={
@@ -78,3 +101,21 @@ app.post("/CreateRoom", (req, res) => {
 
   res.json(data)
 });
+
+app.post("/EnterRoom",(req,res)=>{
+  if(tdb.hasOwnProperty(req.body.name)){
+    let hashedPw=req.body.password;
+    if(tdb[req.body.name].password===hashedPw){
+      if(req.session.hasOwnProperty("authorizedRooms")){
+        req.session.authorizedRooms.push(req.body.name);
+      } else {
+        req.session.authorizedRooms=[req.body.name]
+      }
+      res.send("success")
+    } else {
+      res.send("failure")
+    }
+  } else {
+    res.send("failure")
+  }
+})
